a78c65b010a7b6c043d8b2c686e6d3624aba8833
KouSoulStorm <Arne.Stolze@web.de>
2024-12-16 12:47:34 +0100

    neu

--- src/module-info.java
+++ src/module-info.java
@@ -1,8 +1,40 @@
-/**
- * 
- */
-/**
- * 
- */
-module CCJAVAGRUNDLAGENIB16122024 {
-}
+package grundlagen.Rechner;
+
+public class RechnerProgramme {
+
+	public static void main(String[] args) {
+		int zahl1 = 10;
+		int zahl2 = 20;
+		
+		int Ergebnis = zahl1 + zahl2;
+	System.out.println(" Addition von " + zahl1 + " und " + zahl2 + " ist " + Ergebnis);
+	
+		
+		
+		int ergebnis = zahl2 - zahl1; 
+	
+		System.out.println(" Subtraktion von " + zahl1 + " und " + zahl2 + " ist " +  ergebnis);
+	
+		
+		int rgebnis = zahl2 * zahl1; 
+		System.out.println(" Multi von " + zahl1 + " und " + zahl2 + " ist " + rgebnis);
+		
+	
+		
+
+	}
+	public static void AddierenUndAusgeben (int zahl1, int zahl2) {
+int ergebnis = zahl1 + zahl2;
+System.out.println("Addition von " + zahl1 + " und " + zahl2 + " ist " + ergebnis);
+	}	
+	
+	public static void SubtraktionAusgabe (int zahl1, int zahl2) {
+	int ergebnis = zahl2 - zahl1; 
+	System.out.println(" Subtraktion von " + zahl1 + " und " + zahl2 + " ist " + ergebnis);
+	}
+	
+	public static void MultiAusgabe (int zahl1, int zahl2) {
+		int ergebnis = zahl2 * zahl1; 
+		System.out.println(" Multi von " + zahl1 + " und " + zahl2 + " ist " + ergebnis);
+		
+	
